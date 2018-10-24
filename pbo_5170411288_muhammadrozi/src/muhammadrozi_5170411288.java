7eaa9ddfd5a217734526a62b44ed8194cba2c1ed
muhammadrozi <44402186+muhammadrozi@users.noreply.github.com>
2018-10-24 15:12:56 +0700

    Add files via upload

---
+++ pbo_5170411288_muhammadrozi/src/muhammadrozi_5170411288.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+   import java.util.Scanner;
+public class muhammadrozi_5170411288 {
+  public static void main(String[]args){
+
+int change;
+Scanner s = new Scanner (System.in);
+
+for (change=1; change<=3;change++)
+{
+    System.out.println("1. Input data ");
+    System.out.println("2. Tampil data ");
+    System.out.println("3. Keluar ");
+    System.out.print("Pilihan: "); change = s.nextInt();
+}
+        
+}  
+}
